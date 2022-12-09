4a5bc0abb6f497bc47f7ea0c7d23b086bc96ac77
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2022-12-09 17:04:08 -0300

    avance16

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -36,9 +36,10 @@ function Navbar() {
   }
 
   return (
-    <>
-      {/* <nav className="navbar justify-content-evenly align-content-center navbarMod">
-        <div className="">
+    <NavbarContainer>
+      <ToastContainer />
+      <Wrapper>
+        <LogoContainer>
           <Link to="/" className="p-0 m-0 d-flex align-items-center brand-text">
             <img
               src="./assets/images/logo2.png"
@@ -47,76 +48,43 @@ function Navbar() {
             />
             <h2 className="p-0 m-0 ms-4">Red Solana</h2>
           </Link>
-        </div>
-        <div className="col-5 d-flex flex-row justify-content-around align-items-center">
-          <Link to="/" className="navbar-link m-0 p-0 active">
-            Home
-          </Link>
-          <Link to="/cajeros" className="navbar-link m-0 p-0">
-            Cajeros
-          </Link>
-          <Link to="/tyc" className="navbar-link m-0 p-0 active">
-            Terminos y condiciones
-          </Link>
-
-          {isAdmin ? (
-              <>
-                        <Link to="/admin" className="navbar-link m-0 p-0 active">
-            Admin
-          </Link>
-                       <Button
-              onClick={() => {
-                localStorage.removeItem("active");
-                setIsAdmin(false);
-                logOut();
-              }}
-              variant="danger"
-            >
-              Cerrar sesión
-            </Button>
-              </>
-          ) : (
-            <></>
-          )}
-        </div>
-      </nav> */}
-      <NavbarContainer>
-        <ToastContainer />
-        <Wrapper>
-          <LogoContainer>
+        </LogoContainer>
+        <Menu className={`${isOpenMenu ? "left0" : " "}`}>
+          <MenuItem>
             <Link
               to="/"
-              className="p-0 m-0 d-flex align-items-center brand-text"
+              className="m-0 menuItemLink"
+              onClick={() => setIsOpenMenu(false)}
             >
-              <img
-                src="./assets/images/logo2.png"
-                className="navbar-brand-logo"
-                alt="brand-logo"
-              />
-              <h2 className="p-0 m-0 ms-4">Red Solana</h2>
+              Home
             </Link>
-          </LogoContainer>
-          <Menu className={`${isOpenMenu ? "left0" : " "}`}>
-            <MenuItem>
-              <Link to="/" className="m-0 menuItemLink">
-                Home
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to="/cajeros" className="m-0 menuItemLink">
-                Cajeros
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to="/tyc" className="m-0 menuItemLink">
-                Terminos y condiciones
-              </Link>
-            </MenuItem>
-          </Menu>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to="/cajeros"
+              className="m-0 menuItemLink"
+              onClick={() => setIsOpenMenu(false)}
+            >
+              Cajeros
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to="/tyc"
+              className="m-0 menuItemLink"
+              onClick={() => setIsOpenMenu(false)}
+            >
+              Terminos y condiciones
+            </Link>
+          </MenuItem>
           {isAdmin ? (
             <AdminMenu>
               <MenuItem>
-                <Link to="/admin" className="m-0 menuItemLink">
+                <Link
+                  to="/admin"
+                  className="m-0 menuItemLink"
+                  onClick={() => setIsOpenMenu(false)}
+                >
                   Admin
                 </Link>
               </MenuItem>
@@ -136,17 +104,17 @@ function Navbar() {
           ) : (
             <></>
           )}
-          <HamburguerContainer
-            className={`icon nav-icon-5 ${isOpenMenu ? "open" : " "}`}
-            onClick={() => openMenu()}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </HamburguerContainer>
-        </Wrapper>
-      </NavbarContainer>
-    </>
+        </Menu>
+        <HamburguerContainer
+          className={`icon nav-icon-5 ${isOpenMenu ? "open" : " "}`}
+          onClick={() => openMenu()}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </HamburguerContainer>
+      </Wrapper>
+    </NavbarContainer>
   );
 }
 
@@ -155,7 +123,12 @@ export default Navbar;
 const NavbarContainer = styled.nav`
   width: 100%;
   height: 70px;
-  background-color: #123;
+  background: radial-gradient(
+    circle,
+    rgba(0, 0, 0, 1) 0%,
+    rgba(88, 88, 88, 1) 100%
+  );
+  border-bottom: 1px solid #fff;
   .left0 {
     left: 0 !important;
   }
@@ -170,6 +143,9 @@ const Wrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   margin: auto;
+  @media screen and (max-width: 960px) {
+    padding: 0 20px;
+  }
 `;
 
 const LogoContainer = styled.div`
@@ -188,26 +164,55 @@ const Menu = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
+  @media screen and (min-width: 960px) {
+    align-items: center;
+  }
   @media screen and (max-width: 960px) {
-    background-color: #213123;
+    background-color: #000000;
+    background-image: url("data:image/svg+xml,%3Csvg width='180' height='180' viewBox='0 0 180 180' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M81.28 88H68.413l19.298 19.298L81.28 88zm2.107 0h13.226L90 107.838 83.387 88zm15.334 0h12.866l-19.298 19.298L98.72 88zm-32.927-2.207L73.586 78h32.827l.5.5 7.294 7.293L115.414 87l-24.707 24.707-.707.707L64.586 87l1.207-1.207zm2.62.207L74 80.414 79.586 86H68.414zm16 0L90 80.414 95.586 86H84.414zm16 0L106 80.414 111.586 86h-11.172zm-8-6h11.173L98 85.586 92.414 80zM82 85.586L87.586 80H76.414L82 85.586zM17.414 0L.707 16.707 0 17.414V0h17.414zM4.28 0L0 12.838V0h4.28zm10.306 0L2.288 12.298 6.388 0h8.198zM180 17.414L162.586 0H180v17.414zM165.414 0l12.298 12.298L173.612 0h-8.198zM180 12.838L175.72 0H180v12.838zM0 163h16.413l.5.5 7.294 7.293L25.414 172l-8 8H0v-17zm0 10h6.613l-2.334 7H0v-7zm14.586 7l7-7H8.72l-2.333 7h8.2zM0 165.414L5.586 171H0v-5.586zM10.414 171L16 165.414 21.586 171H10.414zm-8-6h11.172L8 170.586 2.414 165zM180 163h-16.413l-7.794 7.793-1.207 1.207 8 8H180v-17zm-14.586 17l-7-7h12.865l2.333 7h-8.2zM180 173h-6.613l2.334 7H180v-7zm-21.586-2l5.586-5.586 5.586 5.586h-11.172zM180 165.414L174.414 171H180v-5.586zm-8 5.172l5.586-5.586h-11.172l5.586 5.586zM152.933 25.653l1.414 1.414-33.94 33.942-1.416-1.416 33.943-33.94zm1.414 127.28l-1.414 1.414-33.942-33.94 1.416-1.416 33.94 33.943zm-127.28 1.414l-1.414-1.414 33.94-33.942 1.416 1.416-33.943 33.94zm-1.414-127.28l1.414-1.414 33.942 33.94-1.416 1.416-33.94-33.943zM0 85c2.21 0 4 1.79 4 4s-1.79 4-4 4v-8zm180 0c-2.21 0-4 1.79-4 4s1.79 4 4 4v-8zM94 0c0 2.21-1.79 4-4 4s-4-1.79-4-4h8zm0 180c0-2.21-1.79-4-4-4s-4 1.79-4 4h8z' fill='%23d4a747' fill-opacity='0.15' fill-rule='evenodd'/%3E%3C/svg%3E");
     position: absolute;
     top: 70px;
     left: -100%;
     z-index: 1000;
-    width: 100%;
-    height: 90vh;
-    justify-content: center;
+    width: 50%;
+    height: 100vh;
     flex-direction: column;
     align-items: center;
+    gap: 10px;
     transition: all 0.5s ease;
+    border-right: 0.5px solid #fff;
+  }
+  li {
+    &:nth-child(1) {
+      margin-top: 20px;
+      @media screen and (min-width: 960px) {
+        margin: 0;
+      }
+    }
+  }
+  @media screen and (max-width: 475px) {
+    width: 100%;
   }
 `;
 
-const AdminMenu = styled(Menu)`
+const AdminMenu = styled.ul`
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   border: 0.5px solid #d4af37;
   li {
-    &:nth-child(2) {
-      margin: 0;
+    &:first-child {
+      margin-top: 0;
+      margin-bottom: 10px;
+    }
+  }
+  @media screen and (min-width: 960px) {
+    flex-direction: row;
+    li {
+      &:first-child {
+        margin: 0;
+      }
     }
   }
 `;
@@ -226,7 +231,7 @@ const HamburguerContainer = styled.div`
     transition: 0.3s cubic-bezier(0.8, 0.5, 0.2, 1.4);
     width: 100%;
     height: 4px;
-    transition-duration: 500ms;
+    transition-duration: 0.2s;
   }
 
   span {
@@ -243,16 +248,6 @@ const HamburguerContainer = styled.div`
       bottom: 0px;
       left: 0px;
     }
-  }
-
-  &:not(.open):hover span:nth-child(1) {
-    transform: rotate(-3deg) scaleY(1.1);
-  }
-  &:not(.open):hover span:nth-child(2) {
-    transform: rotate(3deg) scaleY(1.1);
-  }
-  &:not(.open):hover span:nth-child(3) {
-    transform: rotate(-4deg) scaleY(1.1);
   }
 
   &.open span:nth-child(1) {
@@ -275,25 +270,28 @@ const HamburguerContainer = styled.div`
 const MenuItem = styled.li`
   margin-right: 10px;
   display: flex;
-  align-items: center;
   transition: all 0.3s ease;
+  color: #d4a747;
   @media screen and (max-width: 960px) {
-    width: 100%;
+    margin: 0;
     justify-content: center;
     align-items: center;
   }
   .menuItemLink {
     background: unset;
+    width: fit-content !important;
     height: fit-content;
+    text-align: center;
     padding: 10px 20px;
     display: flex;
     justify-content: center;
-    align-items: center;
-    color: #d4a747;
+    align-items: flex-start;
     cursor: pointer;
     transition: all 0.5s ease;
+    color: #fff;
+
     &:hover {
-      color: #fff;
+      color: #d4a747;
     }
     @media screen and (max-width: 960px) {
       width: 100px;
--- src/components/readme.md
+++ src/components/readme.md
@@ -1,36 +1,70 @@
 avance1:
-29/11/22: se subio el maquetado basico y se agrego el modal, falta trabajar este. Hasta las 10:30 se trabajaron 3 horas en total.
-avance2:
-29/11/22: se avanzo con el agregado y quitado de cajeros, tambien se llevo a la parte cajeros los cajeros creados. Se configuro los cajeros individuales para renderizar condicionalmente si es mujer u hombre. Se uso react bootstrap para tablas. Se agrega firestore basico.
-Se borro la carpeta data
+    29/11/22: se subio el maquetado basico y se agrego el modal, falta trabajar este. Hasta las 10:30 se trabajaron 3 horas en total.
+avance2: 
+    29/11/22: se avanzo con el agregado y quitado de cajeros, tambien se llevo a la parte cajeros los cajeros creados. Se configuro los cajeros individuales para renderizar condicionalmente si es mujer u hombre. Se uso react bootstrap para tablas. Se agrega firestore basico.
+    Se borro la carpeta data
 avance3:
-29/11/22: - funcion de traer cajeros de la coleccion - funcion de enviar cajeros a la coleccion - se empezo apenas con el auth de usuarios
+    29/11/22:
+        - funcion de traer cajeros de la coleccion
+        - funcion de enviar cajeros a la coleccion
+        - se empezo apenas con el auth de usuarios
 avance4:
-30/11/22: - se estilizo el login, ya esta funcional con 1 usuario. - alertas cuando hay errores como, correo equivocado, contraseña equivocado, muchos intentos. - uso de LocalStorage para guardar sesión - formulario esponsive
-2/12/22: - se configuraron los toast - se configuro loader enn el login - se agrego barra de busquedad - se agrego la funcion de cerrar sesión
+    30/11/22:
+        - se estilizo el login, ya esta funcional con 1 usuario.
+        - alertas cuando hay errores como, correo equivocado, contraseña equivocado, muchos intentos. 
+        - uso de LocalStorage para guardar sesión
+        - formulario esponsive
+    2/12/22:
+        - se configuraron los toast
+        - se configuro loader enn el login
+        - se agrego barra de busquedad
+        - se agrego la funcion de cerrar sesión
 avance5:
-2/12/22: - se avanzo con la busqueda de cajero por nombre - buscador implementado - se actualizaron las funciones de borrar y agregar a firestore
-3/12/22: - se avanzo con el diseño de la pagina inicial
-avance6:
-3/12/22: - se solucionaron warning para deploy en netlify
-//FIXME: - se agrego un boton para ir al formulario de login
+    2/12/22: 
+        - se avanzo con la busqueda de cajero por nombre
+        - buscador implementado
+        - se actualizaron las funciones de borrar y agregar a firestore
+    3/12/22:
+        - se avanzo con el diseño de la pagina inicial
+avance6: 
+    3/12/22: 
+    - se solucionaron warning para deploy en netlify
+    //FIXME: - se agrego un boton para ir al formulario de login
 avance7:
-3/12/22: - saque unos bordes para subirlo a netlify y mostrar al cliente
+    3/12/22:
+        - saque unos bordes para subirlo a netlify y mostrar al cliente
 avance8:
-3/12/22: - archivo redirects en public para redireccionar al actualizar la pagina.
+    3/12/22:
+        - archivo redirects en public para redireccionar al actualizar la pagina.
 avance9:
-4/12/22: - se agrego libreria de animated.css porque aos es mala - se acomodo el formulario de login - se esta testeando los alertas porque se envian 2 teniendo 1 error al completar los campos.
+    4/12/22: 
+        - se agrego libreria de animated.css porque aos es mala
+        - se acomodo el formulario de login
+        - se esta testeando los alertas porque se envian 2 teniendo 1 error al completar los campos.
 avance10:
-4/12/22: - se agregaron animaciones - se esta trabajando en el responsive de la table
+    4/12/22: 
+        - se agregaron animaciones
+        - se esta trabajando en el responsive de la table
 avance11:
-4/12/22: - se trabajo en la responsive de la tabla de admin
+    4/12/22:
+        - se trabajo en la responsive de la tabla de admin
 avance12:
-6/11/22: - se trabajo en el formulario para agregar cajeros, en diseño. Se agrego toast para avisar que se agrego correctamente.
+    6/11/22: 
+        - se trabajo en el formulario para agregar cajeros, en diseño. Se agrego toast para avisar que se agrego correctamente.
 corrección1:
-6/12/22: - me tira un error al darle click a el link de un cajero y no vuelve. busque en internet solucion y agregue la etiqueta <base> al html de public.
+    6/12/22: 
+        - me tira un error al darle click a el link de un cajero y no vuelve. busque en internet solucion y agregue la etiqueta <base> al html de public.
 avance13:
-6/12/22: - tabla responsive
+    6/12/22:
+        - tabla responsive
 avance14:
-6/12/22: - avance con el diseño. - se cambio el loader - se cambio el diseño del login, de los cajeros y de la parte admin
+    6/12/22:
+        - avance con el diseño.
+        - se cambio el loader
+        - se cambio el diseño del login, de los cajeros y de la parte admin
 avance15:
-7/12/22 - navbar responsive
+    7/12/22
+        - navbar responsive
+avance16:
+    9/12/22:
+        - navbar responsive 2
